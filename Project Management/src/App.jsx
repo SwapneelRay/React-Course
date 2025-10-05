b95daf678a1949b9b8f687061788ac6b29a990bc
SwapneelRay <46602995+SwapneelRay@users.noreply.github.com>
2025-10-05 10:29:24 +0530

    initial commit
    project management

--- Project Management/src/App.jsx
+++ Project Management/src/App.jsx
@@ -1,8 +1,49 @@
+import { useState } from "react";
+import NoProjectSelected from "./Components/NoProjectSelected";
+import Project from "./Components/Project";
+import Sidebar from "./Components/Sidebar";
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    project: [],
+  });
+
+  function startAddProjectHandler() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function createProjectHandler(data) {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...data,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        project: [...prevState.project, newProject],
+      };
+    });
+  }
+
+  let content;
+console.log(projectState)
+  if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={startAddProjectHandler} />;
+  } else {
+    if (projectState.selectedProjectId === null) {
+      content = <Project  onSaveClick={createProjectHandler} />;
+    }
+  }
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <Sidebar project={projectState} onStartAddProject={startAddProjectHandler}></Sidebar>
+      {content}
+    </main>
   );
 }
 
---
+++ Project Management/src/Components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import Button from "./Button";
+
+export default function Sidebar({onStartAddProject}) {
+  return (
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+        your projects
+      </h2>
+      <Button onClick={onStartAddProject}>+ Add Project</Button>
+      <ul className="mt-8">
+        <li>
+          <button className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800">
+            
+          </button>
+        </li>
+      </ul>
+    </aside>
+  );
+}
